cffcd7da77f4f8d048a78b641f49bfc87f919f1e
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-09 21:39:11 +0600

    Job category data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("jobCategory.json")
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  }, []);
+
+  console.log(data);
   return (
     <div className="App">
-      <h1 className="font-bold">Hello world!</h1>
+      <h1>Hello world</h1>
+      {data.map((job) => (
+        <img src={job.img} alt="" />
+      ))}
     </div>
   );
 }
