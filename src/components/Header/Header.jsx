645602146e79bf9acfbd223482dd7fa49a1edd1b
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-10 17:56:58 +0600

    Navbar added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <Link to="/">Home</Link>
-      <Link to="blog">Blog</Link>
+    <div className="w-full md:w-9/12 my-10 mx-auto flex justify-between items-start">
+      <h1 className="font-bold text-4xl">GetAJob</h1>
+      <nav className="flex gap-10 items-center ">
+        <NavLink to="/" className="hover:text-violet-400">
+          Home
+        </NavLink>
+        <NavLink to="stats" className="hover:text-violet-400">
+          Statistics
+        </NavLink>
+        <NavLink to="job" className="hover:text-violet-400">
+          Applied Job
+        </NavLink>
+        <NavLink to="blog" className="hover:text-violet-400">
+          Blog
+        </NavLink>
+      </nav>
+      <button className="font-bold bg-violet-400 py-3 px-4 text-white rounded-md">
+        Start Applying
+      </button>
     </div>
   );
 };
